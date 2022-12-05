25b3a4967f35d41fd3c08a8c3d39e0444fa18262
Alexander Görtz <alex@nyloc.de>
2022-12-05 07:52:30 +0100

    Solution for day 4

---
+++ day_04.py
@@ -0,0 +1,15 @@
+#  --- Day 4: Camp Cleanup ---
+# %% Initialization
+
+# %% Read input
+with open("day_04_input.txt") as data:
+    assignments = [tuple(map(int, x.replace("-", ",").split(","))) for x in data]
+
+# %% Part 1
+print(sum(
+    (a >= c and b <= d) if b - a <= d - c else (c >= a and d <= b) for a, b, c, d in
+    assignments))
+
+# %% Part 2
+print(sum((d >= a >= c or d >= b >= c or b >= c >= a or b >= d >= a) for a, b, c, d in
+          assignments))
