9cfaa4c72f2cad4a9e730a98f7d4e8ca4bf732fe
Alexander Görtz <alex@nyloc.de>
2022-12-05 08:06:38 +0100

    Simplify count conditions

--- day_04.py
+++ day_04.py
@@ -6,8 +6,7 @@ with open("day_04_input.txt") as data:
     assignments = [tuple(map(int, x.replace("-", ",").split(","))) for x in data]
 
 # %% Part 1
-print(sum((a >= c and b <= d) or (c >= a and d <= b) for a, b, c, d in assignments))
+print(sum(a <= c <= d <= b or c <= a <= b <= d for a, b, c, d in assignments))
 
 # %% Part 2
-print(sum(d >= a >= c or d >= b >= c or b >= c >= a or b >= d >= a for a, b, c, d in
-          assignments))
+print(sum(a <= d and b >= c for a, b, c, d in assignments))
