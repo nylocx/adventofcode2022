a59220ce28f2c0ecbfa080d49f34d38f10f5e6ba
Alexander Görtz <alex@nyloc.de>
2022-12-05 08:18:03 +0100

    Add some text to the printouts

--- day_04.py
+++ day_04.py
@@ -3,10 +3,11 @@
 
 # %% Read input
 with open("day_04_input.txt") as data:
-    assignments = [tuple(map(int, x.replace("-", ",").split(","))) for x in data]
+    assignments = [[*map(int, x.replace("-", ",").split(","))] for x in data]
 
 # %% Part 1
-print(sum(a <= c <= d <= b or c <= a <= b <= d for a, b, c, d in assignments))
+print("Contained assignments:",
+      sum(a <= c <= d <= b or c <= a <= b <= d for a, b, c, d in assignments))
 
 # %% Part 2
-print(sum(a <= d and b >= c for a, b, c, d in assignments))
+print("Overlapping assignments:", sum(a <= d and b >= c for a, b, c, d in assignments))
