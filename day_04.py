ecf0eca34cd3caa614ad9fdf55adea1104965221
Alexander Görtz <alex@nyloc.de>
2022-12-05 07:56:53 +0100

    Remove unnecessary if else

--- day_04.py
+++ day_04.py
@@ -6,10 +6,8 @@ with open("day_04_input.txt") as data:
     assignments = [tuple(map(int, x.replace("-", ",").split(","))) for x in data]
 
 # %% Part 1
-print(sum(
-    (a >= c and b <= d) if b - a <= d - c else (c >= a and d <= b) for a, b, c, d in
-    assignments))
+print(sum((a >= c and b <= d) or (c >= a and d <= b) for a, b, c, d in assignments))
 
 # %% Part 2
-print(sum((d >= a >= c or d >= b >= c or b >= c >= a or b >= d >= a) for a, b, c, d in
+print(sum(d >= a >= c or d >= b >= c or b >= c >= a or b >= d >= a for a, b, c, d in
           assignments))
