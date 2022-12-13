16abc76559e99581fa8c97d1d0e1699aeccfb7f3
Alexander Görtz <alex@nyloc.de>
2022-12-13 13:26:58 +0100

    Clean up compare function

--- day_13.py
+++ day_13.py
@@ -5,23 +5,22 @@ from functools import cmp_to_key
 
 
 def compare(a, b):
-    # print("Comparing", a, "vs", b)
     if a is None:
-        return True
+        return -1
     elif b is None:
-        return False
+        return 1
     elif isinstance(a, int) and isinstance(b, int):
-        return a < b if a != b else None
+        return -1 if a < b else 1 if b < a else 0
     elif isinstance(a, int) and isinstance(b, list):
         return compare([a], b)
     elif isinstance(a, list) and isinstance(b, int):
         return compare(a, [b])
     else:
         for x, y in zip_longest(a, b):
-            if (r := compare(x, y)) is None:
+            if (r := compare(x, y)) == 0:
                 continue
-            else:
-                return r
+            return r
+        return 0
 
 
 # %% Read input
@@ -31,14 +30,12 @@ with open("day_13_input.txt") as data:
 # %% Part 1
 print(
     "Sum of wrong ordered pairs:",
-    sum(i + 1 for i, (a, b) in enumerate(pairs) if compare(a, b)),
+    sum(i + 1 for i, (a, b) in enumerate(pairs) if compare(a, b) < 0),
 )
 
 # %% Part 2
 packets = [x for y in pairs for x in y] + [[[2]], [[6]]]
-sorted_packages = sorted(
-    packets, key=cmp_to_key(lambda a, b: -1 if compare(a, b) else 1)
-)
+sorted_packages = sorted(packets, key=cmp_to_key(compare))
 print(
     "Decoder Key:",
     (sorted_packages.index([[2]]) + 1) * (sorted_packages.index([[6]]) + 1),
